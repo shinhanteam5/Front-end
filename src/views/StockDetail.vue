6c6e0bd44bf209dc04a6a7172e51ab18cc74dbdc
yewonbahn <banyaewon98@gmail.com>
2023-02-21 14:28:17 +0900

    feat: stockDetail 추가

--- src/views/StockDetail.vue
+++ src/views/StockDetail.vue
@@ -9,17 +9,25 @@
         <!-- 리스트 기준 -->
         
         <!-- 종목 리스트 -->
-        <div> 
+        <div class="main"> 
+
             {{detail[0].name}}
         </div>
         <div> 
             {{detail[0].current_price}}
         </div>
-        <div class="chart"> 
-          <button>1주</button>
-          <button>3달</button>
-          <button>1년</button>
-          <button>3년</button>
+        <div> 
+            + {{detail[0].earn}} ({{detail[0].earn_rate}}%) 오늘
+        </div>
+
+        <div class="chartbutton">
+          <img  :src="imgurl"/>
+          <ul class="tab" ref="tab" @click.prevent="setContext">
+            <li class="on"><a>1주</a></li>
+            <li><a>한달</a></li>
+            <li><a>1년</a></li>
+            <li><a>3년</a></li>
+         </ul>
         </div>
 
       </div>
@@ -36,11 +44,42 @@
   export default {
     data() {
       return {
-        detail: [],
+        imgurl:"",
+        chartContext:"1주",
+        detail: [{}],
+        imageMatch:{
+              "1주":"week",
+              "한달":"month3",
+              "1년":"year",
+              "3년":"year3",
+
+            }
       };
     },
   
-    methods: {},
+    methods: {
+      setContext(ev){
+        this.chartContext=ev.target.textContent;
+        this.setCrrTab(ev.target.parentNode) ; // li가 들어감.
+      },
+      setCrrTab(crr){
+        this.$refs.tab.querySelectorAll('li').forEach((item)=>{
+          item.classList.remove('on')
+        })
+        this.$refs.tab.querySelectorAll('a').forEach((item)=>{
+
+          if(item.textContent===crr.textContent){
+            
+            var img = this.imageMatch[crr.textContent]
+            this.imgurl=this.detail[0][img]
+            console.log()
+            // this.imgurl=this.detail[0].this.imageMatch[crr.textContent]
+            item.parentNode.classList.add('on');
+          }
+        })
+
+      }
+    },
   
     created() {
       axios
@@ -48,10 +87,10 @@
           'http://127.0.0.1:8000/api/stocklist/377300'
         )
         .then((response) => {
+        
           this.detail=response.data
+          this.imgurl=response.data[0].week
         });
-
-
     },
   };
   </script>
